5a5d009b3aea798dc2b0c177604fe336505fbc4c
gitchange <change.wuu@gmail.com>
2015-06-06 21:27:01 +0800

    20150606H1

---
+++ ConsoleAppSolar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SolarTerm st = new SolarTerm();
+            st.JQtest(2015);
+            st.paiYue(2015);
+        }
+    }
+}
